132c7fa73aa9bac34456cf05ffa30ffd8681d77c
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-03-31 16:06:53 +0700

    memasukan dengan input void dan menggunakan while

--- QuickSort_063/QuickSort_063.cpp
+++ QuickSort_063/QuickSort_063.cpp
@@ -6,3 +6,15 @@ int arr[20];
 int cmp_count = 0; // number of comparasiom
 int mov_count = 0; // number of data movement
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMasukan panjang array adalah 0" << endl;
+	}
